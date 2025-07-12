8e9259e215b52dbd632dd87410f247c260916d85
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-12 20:43:28 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
+import Programs from './Components/Programs/Programs'
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <div className="container">
+        <Programs/>
         <About/>
       </div>
     </div>
