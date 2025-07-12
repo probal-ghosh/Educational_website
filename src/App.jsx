b76b4433587e172cee36f7f497a03d902104b3c5
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-12 20:41:03 +0530

    fixed some minor bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import About from './Components/About/About'
 
 
 const App = () => {
@@ -8,7 +9,9 @@ const App = () => {
     <div>
       <Navbar/>
       <Hero/>
-      
+      <div className="container">
+        <About/>
+      </div>
     </div>
   )
 }
